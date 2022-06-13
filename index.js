461ea4e04e8f6865973867ab65fb60e3fb429f1a
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-13 13:55:43 +0300

    JavaScript Event Listeners Lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
+/*function addingEventListener() {
+    const input=document.getElementById('input');
+    input.addEventListener('click',function(){
+        alert('I was clicked!');
+    });
+    return input
 }
+addingEventListener()*/
+
+const input=document.getElementById('input');
+
+function clickAlert(){
+    alert('I was clicked!');
+}
+function addingEventListener(){
+    input.addEventListener('click',clickAlert);
+    
+}
+addingEventListener()
+
+
